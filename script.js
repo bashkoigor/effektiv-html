1695fc7bcd41eccb760b8ed56d7487540f9e5508
admin <admin@mail.loc>
2022-04-22 16:34:52 +0200

    init index page

--- script.js
+++ script.js
@@ -4,38 +4,52 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 });
 
-// Product Carousel
-const productCarousel = new Carousel(document.querySelector("#product-carousel"), {
-    Dots: false,
-});
-const thumbCarousel = new Carousel(document.querySelector("#product-thumb-carousel"), {
-    Sync: {
-        target: productCarousel,
-        friction: 0,
-    },
-    Dots: false,
-    Navigation: false,
-    center: true,
-    slidesPerPage: 1,
-    infinite: false,
-});
-// Carousel block
-const galleryCarousel = new Carousel(document.querySelector("#gallery-carousel"), {
-    slidesPerPage : 1,
-    center : false
-});
-Fancybox.bind("#gallery-carousel a", {
-    caption: function (fancybox, carousel, slide) {},
-});
-// Other blocks
-const inColorCarousel = new Carousel(document.querySelector("#in-color-carousel"), {
-    slidesPerPage : 1,
-    center : false
-});
-Fancybox.bind("#in-color-carousel a", {
-    caption: function (fancybox, carousel, slide) {},
-});
+// For main page
+if (document.querySelector(".main-page")) {
+    // Main Carousel
+    // const mainCarousel = new Carousel(document.querySelector("#main-carousel"), {
+    //     slidesPerPage : 1,
+    //     center : true,
+    //     Dots: false
+    // });
+}
 
+// For product page
+if (document.querySelector(".product-page")) {
+    // Product Carousel
+    const productCarousel = new Carousel(document.querySelector("#product-carousel"), {
+        Dots: false,
+    });
+    const thumbCarousel = new Carousel(document.querySelector("#product-thumb-carousel"), {
+        Sync: {
+            target: productCarousel,
+            friction: 0,
+        },
+        Dots: false,
+        Navigation: false,
+        center: true,
+        slidesPerPage: 1,
+        infinite: false,
+    });
+    // Carousel block
+    const galleryCarousel = new Carousel(document.querySelector("#gallery-carousel"), {
+        slidesPerPage : 1,
+        center : false
+    });
+    Fancybox.bind("#gallery-carousel a", {
+        caption: function (fancybox, carousel, slide) {},
+    });
+    // Other blocks
+    const inColorCarousel = new Carousel(document.querySelector("#in-color-carousel"), {
+        slidesPerPage : 1,
+        center : false
+    });
+    Fancybox.bind("#in-color-carousel a", {
+        caption: function (fancybox, carousel, slide) {},
+    });
+}
+
+// Mobile style
 window.onscroll = function() {
     if (window.pageYOffset == 0) {
         document.getElementById("to-top").style.display = "none";
@@ -50,6 +64,7 @@ window.onscroll = function() {
     }
 }
 
+// Scroll to top
 document.getElementById("to-top").onclick = function() { scrollToTop() };
 function scrollToTop() {
     window.scrollTo({
@@ -57,4 +72,3 @@ function scrollToTop() {
         behavior: 'smooth'
     });
 }
-
